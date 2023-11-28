263e1218fa27e6c61defabd01a9031ed109d7c0c
Yash Mastud <mastud.yash7@gmail.com>
2023-11-28 14:50:43 +0530

    learnt datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello")
+// alert(3+3) //we are using node.js and not browser
+
+let name = "yash"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to the power 53
+//bigint
+//string => ""
+//boolean true/false
+//null => standalone value
+//undefined => value not assigned yet
+//symbol => unique
+
+//object
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
