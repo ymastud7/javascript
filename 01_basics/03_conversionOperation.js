96fa9b76c76e06766cb7711342c1f02cc19812c5
Yash Mastud <mastud.yash7@gmail.com>
2023-11-28 19:02:50 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = "33"
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1, false => 0
+
+let isLoggedIn = "yash"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "yash" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
